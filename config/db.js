6e9c7142385a717f4b958ec1cdb6f92245b4769a
Hani-hue <isnahabibah9827@gmail.com>
2026-07-22 14:35:19 +0700

    tambah fungsi koneksi dan sinkronisasi database

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const db = require('../models');
+
+async function connectToDatabase() {
+  try {
+    await db.sequelize.authenticate();
+    console.log('Connection to the database has been established successfully.');
+
+    await db.sequelize.sync({ alter: true });
+    console.log('Database synchronized successfully.');
+    
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
