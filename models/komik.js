fd146ee6531a9a48a1e52abadd5f718c0d1a3bdb
Hani-hue <isnahabibah9827@gmail.com>
2026-07-22 14:43:54 +0700

    tambah field id, title, description, author di komik.js

--- models/komik.js
+++ models/komik.js
@@ -1,1 +1,20 @@
-module.exports = (sequelize, DataTypes) => {};
+module.exports = (sequelize, DataTypes) => {
+    const Komik = sequelize.define("Komik", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    title: {
+        type: DataTypes.STRING,
+    },
+    description: {
+        type: DataTypes.TEXT,
+    },  
+    author: {
+        type: DataTypes.STRING,
+    }
+    });
+    return Komik;
+
+};
